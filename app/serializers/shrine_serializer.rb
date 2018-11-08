b78b73a6bed9c28fc844c1002a7311f61c5dbe18
Chris Stull <chris.stull@gmail.com>
2018-11-08 15:37:29 -0500

    JK fixed the serializers and models

--- app/serializers/shrine_serializer.rb
+++ app/serializers/shrine_serializer.rb
@@ -1,6 +1,7 @@
 class ShrineSerializer < ActiveModel::Serializer
   attributes(*Shrine.attribute_names.map(&:to_sym))
   belongs_to :user
+  belongs_to :back
   belongs_to :door
   has_many :offerings
   has_many :items, through: :offerings
