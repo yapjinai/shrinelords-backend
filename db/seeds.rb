9297a735fa186bc3bbfc84a5c29bbfd24a0a0049
yapjinai <jinai.jyap@gmail.com>
2018-11-09 15:18:26 -0500

    fixed seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,84 +12,92 @@
 # name: 'Luke', movie: movies.first
 # )
 
-Door.create(
+Door.delete_all
+Shrine.delete_all
+Item.delete_all
+User.delete_all
+Offering.delete_all
+Back.delete_all
+
+u1 = User.create(
+  name: "The Ubiquitous Sean Para",
+  password: "seanpara",
+  avatar: "https://ca.slack-edge.com/T02MD9XTF-UCD89E1T5-d95a13a8ed69-48",
+  birthday: "May 27, 1994"
+)
+
+d1 = Door.create(
   name: "default",
   closed_img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Closed_door_02.jpg/800px-Closed_door_02.jpg",
   open_img: "http://www.handballtunisie.org/upload/2018/03/05/grand-open-double-doors-open-double-doors-obobkebumennewsco-gateways-open-doors-open-double-doors-l-d535fd5360b651d9.jpg"
 )
-Door.create(
+d2 = Door.create(
   name: "cabinet",
   closed_img: "https://www.graindesigns.com/wp-content/uploads/2017/02/Custom-Wood-Printer-Stand-with-Sliding-Doors-Closed.jpg",
   open_img: "https://www.graindesigns.com/wp-content/uploads/2017/02/Custom-Wood-Printer-Stand-with-Sliding-Doors-Open.jpg"
 )
-Door.create(
+d3 = Door.create(
   name: "japanese",
   closed_img: "https://img.alicdn.com/imgextra/i1/1906043003/TB22RmauORnpuFjSZFCXXX2DXXa_!!1906043003.jpg",
   open_img: "https://c.pxhere.com/photos/3e/87/japan_Japanese_ryokan_sliding_doors_tatami_floors_window-1106912.jpg!d"
 )
 
-Shrine.create(
-  user_id: 1,
-  door_id: 1,
-  back_id: 1,
+b1 = Back.create(
+  name: "default",
+  video: "https://www.youtube.com/watch?v=S0bk3eAPMKk"
+)
+b2 = Back.create(
+  name: "water",
+  video: "./assets/video/sparkling_ocean_waves.mp4"
+)
+
+s1 = Shrine.create(
+  user: u1,
+  door: d1,
+  back: b1,
   name: "default",
   views: 0,
   idle_views: 0
 )
 
-Item.create(
+i1 = Item.create(
   name: "dark candelabra",
   image: "https://modishstore.imgix.net/s/files/1/0236/1829/products/Roost-Dark-Driftwood-Candelabra_-Horizontal.jpg?v=1495590131&auto=compress,format&w=1200&"
 )
-Item.create(
+i2 = Item.create(
   name: "flower",
   image: "https://i.imgur.com/80awuFg.png"
 )
-Item.create(
+i3 = Item.create(
   name: "virgin mary candle",
   image: "https://i.imgur.com/Ud0CZhY.pnghttps://i.imgur.com/Ud0CZhY.png"
 )
-Item.create(
+i4 = Item.create(
   name: "basic candle",
   image: "https://i.imgur.com/l4jclFo.png"
 )
-Item.create(
+i5 = Item.create(
   name: "candle flame gif",
   image: "https://i.imgur.com/GgrHPm7.gif"
 )
-Item.create(
+i6 = Item.create(
   name: "incense",
   image: "https://i.imgur.com/DOacUuy.png"
 )
 
-Offering.create(
-  shrine_id: 1,
-  item_id: 1,
-  style: ""
-)
-Offering.create(
-  shrine_id: 1,
-  item_id: 2,
+o1 = Offering.create(
+  shrine: s1,
+  item: i1,
   style: '{"style":{"top":"20%","left":"50%"}}'
 )
-Offering.create(
-  shrine_id: 1,
-  item_id: 3,
+
+o2 = Offering.create(
+  shrine: s1,
+  item: i2,
+  style: '{"style":{"top":"20%","left":"50%"}}'
+)
+o3 = Offering.create(
+  shrine: s1,
+  item: i3,
   style: '{"style":{"top":"60%","left":"10%"}}'
-)
-
-Back.create(
-  name: "default",
-  video: "https://www.youtube.com/watch?v=S0bk3eAPMKk"
-)
-Back.create(
-  name: "water",
-  video: "./assets/video/sparkling_ocean_waves.mp4"
-)
-
-User.create(
-  name: "The Ubiquitous Sean Para",
-  password: "seanpara",
-  avatar: "https://ca.slack-edge.com/T02MD9XTF-UCD89E1T5-d95a13a8ed69-48",
-  birthday: "May 27, 1994"
 )
