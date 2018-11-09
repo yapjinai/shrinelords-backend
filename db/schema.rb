f9e33cebfdb3340739bd1863a846feb9a3285910
Chris Stull <chris.stull@gmail.com>
2018-11-09 13:57:03 -0500

    Added styles as jsonb to offerings with additional seeds.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_08_194534) do
+ActiveRecord::Schema.define(version: 2018_11_09_183450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2018_11_08_194534) do
     t.integer "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.jsonb "style"
   end
 
   create_table "shrines", force: :cascade do |t|
